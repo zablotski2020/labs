1c8e44c37dffe09d3fcd24a69b8b17276652a59b
zablotski2020 <72109229+zablotski2020@users.noreply.github.com>
2020-10-19 00:29:48 +0300

    new file

---
+++ bag.cpp
@@ -0,0 +1,7 @@
+#include "bag.hpp"
+#include <iostream>
+using namespace std;
+
+int main{
+    cout << "ssss" << endl;
+}
---
+++ bag.hpp
@@ -0,0 +1,13 @@
+//
+//  bag.hpp
+//  
+//
+//  Created by Ilya on 10/19/20.
+//
+
+#ifndef bag_hpp
+#define bag_hpp
+
+#include <stdio.h>
+
+#endif /* bag_hpp */
